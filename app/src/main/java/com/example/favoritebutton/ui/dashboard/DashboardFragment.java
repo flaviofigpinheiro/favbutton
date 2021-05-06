8e9207c0d210cf8069237b4e0105c6f89f820b11
Flavio <Flaviofigpinheiro@gmail.com>
2021-05-06 11:31:30 +0100

    commit 1

---
+++ app/src/main/java/com/example/favoritebutton/ui/dashboard/DashboardFragment.java
@@ -0,0 +1,75 @@
+// ok
+
+package com.example.favoritebutton.ui.dashboard;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.favoritebutton.ui.FavAdapter;
+import com.example.favoritebutton.ui.FavDB;
+import com.example.favoritebutton.ui.FavItem;
+
+import com.example.favoritebutton.ui.R;
+
+
+public class DashboardFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    private FavDB favDB;
+    private List<FavItem> favItemList = new ArrayList<>();
+    private FavAdapter favAdapter;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+
+        favDB = new FavDB(getActivity());
+        recyclerView = root.findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+       loadData();
+
+        return root;
+    }
+
+    private void loadData() {
+        if (favItemList != null) {
+            favItemList.clear();
+        }
+        SQLiteDatabase db = favDB.getReadableDatabase();
+        Cursor cursor = favDB.select_all_favorite_list();
+        try {
+            while (cursor.moveToNext()) {
+                String title = cursor.getString(cursor.getColumnIndex(FavDB.ITEM_TITLE));
+                String id = cursor.getString(cursor.getColumnIndex(FavDB.KEY_ID));
+                int image = Integer.parseInt(cursor.getString(cursor.getColumnIndex(FavDB.ITEM_IMAGE)));
+                FavItem favItem = new FavItem(title, id, image);
+                favItemList.add(favItem);
+            }
+        } finally {
+            if (cursor != null && cursor.isClosed())
+                cursor.close();
+            db.close();
+        }
+
+        favAdapter = new FavAdapter(getActivity(), favItemList);
+
+        recyclerView.setAdapter(favAdapter);
+
+    }
+
+}
